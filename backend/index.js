52d1a88436cd0b2acf28fd98c771a33162a8ac0e
Harkaran Sohal <soha0021@algonquinlive.com>
2024-05-25 18:45:28 -0400

    Added a .env file to the backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express();
-const PORT = 5000;
+require('dotenv').config();
+const PORT = process.env.PORT || 5000
 
 app.post('/test-webhook', async (req, res) => {
     console.log(req.body)
