d39e5a973d5a784626be4d56598dde150031446c
Harkaran Sohal <soha0021@algonquinlive.com>
2024-05-25 20:10:12 -0400

    Added a middleware that verifies the webhook request from clerk

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
-const express = require("express")
+const express = require("express");
 const app = express();
-require('dotenv').config();
-const PORT = process.env.PORT || 5000 
+require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+const verifyWebhook = require("./middlewares/verifyWebhook");
 
-app.post('/test-webhook', async (req, res) => {
-    console.log(req.body)
-    return res.status(201).json({success: "ok"})
-})
+
+app.post("/test-webhook", verifyWebhook, async (req, res) => {
+  console.log(req.body);
+  return res.status(201).json({ success: "ok" });
+});
 
 app.listen(PORT, () => {
-    console.log("App is up on port "+PORT)
-})
+  console.log("App is up on port " + PORT);
+});
